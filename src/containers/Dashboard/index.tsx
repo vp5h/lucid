0da21750e26d139dc68e72e21d6e8d6ab5ec1b66
vp5h <praveshvyas.18@gmail.com>
2024-02-23 04:40:39 +0530

    removes hardcoded stuff

--- src/containers/Dashboard/index.tsx
+++ src/containers/Dashboard/index.tsx
@@ -7,6 +7,8 @@ import "./index.css";
 import Modal from "../../components/Modal";
 import { Stats } from "../../components/Stats";
 
+// TODO add types
+
 const DashBoard = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedRow, setSelectedRow] = useState({});
@@ -20,55 +22,12 @@ const DashBoard = () => {
   const { isPending, error, data } = useQuery({
     queryKey: ["STOCK"],
     staleTime: Infinity,
-    queryFn: () => {
-      return [
-        {
-          name: "Bluetooth",
-          category: "Electronic",
-          value: "$150",
-          quantity: 5,
-          price: "$30",
-          isEnabled: true,
-        },
-        {
-          name: "Edifier M43560",
-          category: "Electronic",
-          value: "0",
-          quantity: 0,
-          price: "$0",
-          isEnabled: true,
-        },
-        {
-          name: "Sony 4k ultra 55 inch TV",
-          category: "Electronic",
-          value: "$1190",
-          quantity: 17,
-          price: "$70",
-          isEnabled: true,
-        },
-        {
-          name: "Samsumg 55 inch TV",
-          category: "Electronic",
-          value: "$600",
-          quantity: 50,
-          price: "$12",
-          isEnabled: true,
-        },
-        {
-          name: "samsumg S34 Ultra",
-          category: "phone",
-          value: "$0",
-          quantity: 0,
-          price: "$0",
-          isEnabled: true,
-        },
-      ];
-      //   setTableData()
-    },
-    // queryFn: () =>
-    //   fetch("https://dev-0tf0hinghgjl39z.api.raw-labs.com/inventory").then(
-    //     (res) => res.json()
-    //   ),
+    queryFn: () =>
+      fetch("https://dev-0tf0hinghgjl39z.api.raw-labs.com/inventory")
+        .then((res) => res.json())
+        .then((resp) => {
+          return resp.map((each) => ({ ...each, isEnabled: true }));
+        }),
   });
   useEffect(() => {
     if (role === "2") {
